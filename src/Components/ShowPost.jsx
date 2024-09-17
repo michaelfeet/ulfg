6becca2ac9eddb51c07e207458132a3035a3d482
michaelfeet <michael.a.feet@gmail.com>
2024-09-17 02:18:11 -0400

    refactor showpost err

--- src/Components/ShowPost.jsx
+++ src/Components/ShowPost.jsx
@@ -1,25 +1,15 @@
-import { Box, Container, CssBaseline, ThemeProvider, Typography } from '@mui/material';
-import { Loading } from './Loading';
-import { ShowPostTernary } from './ShowPostTernary';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { showPostCompareTheme } from './themes/showPostCompareTheme';
-import { outerBox } from './StyledComponents/OuterBox';
+import { ErrorMessage } from './ErrorMessage';
+import { ShowPostCard } from './ShowPostCard';
 
 export const ShowPost = (props) => {
     return (
         <ThemeProvider theme={showPostCompareTheme}>
             <CssBaseline />
             {!props.post
-                ? <Typography variant='h3' sx={{ pt: 7 }}>
-                    Post Not Found
-                </Typography>
-                : <Container fixed maxWidth='xl'>
-                    <Box sx={{ mt: 6, mb: 10, ...outerBox }}>
-                        {!props.post.user
-                            ? <Loading loading={props.loading} />
-                            : <ShowPostTernary post={props.post} user={props.user} />
-                        }
-                    </Box>
-                </Container>}
+                ? <ErrorMessage error={'Post not found'} />
+                : <ShowPostCard post={props.post} user={props.user} loading={props.loading} setLoading={props.setLoading} />}
         </ThemeProvider>
     );
 }
